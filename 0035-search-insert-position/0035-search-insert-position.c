f7ffe86607b0f155b745d7c4ebf407503a83b899
PRANAV SP <PES1UG24CS337@stu.pes.edu>
2026-07-07 21:26:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (36.01%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,20 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    
+    int l=0;
+    int r=numsSize-1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(target==nums[mid]){
+            return mid;
+        }
+        else if(target>nums[mid]){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+    }
+    return l;
+    
+}
+    

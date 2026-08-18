4995330aec1adb58a099320045a93628fd261677
PRANAV SP <PES1UG24CS337@stu.pes.edu>
2026-08-18 20:20:17 +0530

    Time: 7 ms (29.2%), Space: 21 MB (69.6%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        output=0
+        for i in range(len(nums)):
+            output=output^nums[i]
+        return output
+        

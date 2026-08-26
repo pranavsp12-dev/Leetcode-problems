570eb8665bf572f245db40b5e50114c52057817d
PRANAV SP <PES1UG24CS337@stu.pes.edu>
2026-08-26 17:48:00 +0530

    Time: 43 ms (33.5%), Space: 31.3 MB (74.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,5 +5,7 @@ class Solution:
         for i in range(1,len(nums)):
             cursum=max(cursum+nums[i],nums[i])
             maxsum=max(maxsum,cursum)
+
+    
         return maxsum
-        
+        #kadane's algo- if the sum is less then the current element then start the subarray from this new element. else just keep on adding to find the sum.
